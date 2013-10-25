f9e7bc7967f51d8af4472d33378e9d7648161056
Matt Gray <matt@mattgray.ca>
2013-10-25 18:32:26 -0400

    class CRM

--- summit.rb
+++ summit.rb
@@ -1,24 +1,25 @@
-def print_main_menu
-  puts "[1] Add a new contact"
-  puts "[2] Modify an existing contact"
-  puts "[3] Delete a contact"
-  puts "[4] Display all the contacts"
-  puts "[5] Display an attribute"
-  puts "[6] Exit"
-  puts "Enter a number:"
-end
+class CRM
+  def print_main_menu
+    puts "[1] Add a new contact"
+    puts "[2] Modify an existing contact"
+    puts "[3] Delete a contact"
+    puts "[4] Display all the contacts"
+    puts "[5] Display an attribute"
+    puts "[6] Exit"
+    puts "Enter a number:"
+  end
 
-def main_menu
-  print_main_menu
-  user_selected = gets.to_i
-  call_option(user_selected)
-end
+  def main_menu
+    print_main_menu
+    user_selected = gets.to_i
+    call_option(user_selected)
+  end
 
-def call_option(user_selected)
-  add_new_contact if user_selected == 1
-  modify_existing_contact if user_selected == 2
-  delete_contact if user_selected == 3
-  display_all_contacts if user_selected == 4
-  display_an_attribute if user_selected == 5
-  exit if user_selected == 6
-end
+  def call_option(user_selected)
+    add_new_contact if user_selected == 1
+    modify_existing_contact if user_selected == 2
+    delete_contact if user_selected == 3
+    display_all_contacts if user_selected == 4
+    display_an_attribute if user_selected == 5
+    exit if user_selected == 6
+  end
